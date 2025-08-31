8354f049bcab983111af96075888f3df0303421c
mayank <120419133+Mayankk0608@users.noreply.github.com>
2025-08-31 17:22:29 +0530

    Update Docker_Push.groovy

--- vars/Docker_Push.groovy
+++ vars/Docker_Push.groovy
@@ -1,3 +1,4 @@
+
 void call(String Project, String ImageTag, String dockerHubUser){
   withCredentials([usernamePassword(credentialsId:'Dockerhubcred' ,passwordVariable:'dockerHubPass' ,usernameVariable:'dockerHubUser')]){
     sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
